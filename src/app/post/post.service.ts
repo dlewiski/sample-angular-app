057e19a09bfcb6d4acd64281a447e1add9e5923b
David Lewis <davidlewisp@gmail.com>
2018-05-15 11:48:30 -0700

    add posts service to retreive information from rails api

---
+++ src/app/post/post.service.ts
@@ -0,0 +1,4 @@
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import { Post } from './post';
---
+++ src/app/post/post.ts
@@ -0,0 +1,8 @@
+export class Post {
+  constructor(
+    public id?: number,
+    public title?: number,
+    public description?: number,
+    public updated_at?: number,
+  ) {}
+}
